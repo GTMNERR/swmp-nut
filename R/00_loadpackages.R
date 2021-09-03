595462e1c113d41027456442eb6b40a2c5f42709
Dunnigan <Shannon.Dunnigan@dep.state.fl.us>
2021-09-03 16:13:32 -0400

    updated and cleaned loadpackages

--- R/00_loadpackages.R
+++ R/00_loadpackages.R
@@ -1,13 +1,30 @@
-library(lubridate)
-library(tidyverse) # cheat to load ggplot2, dplyr, tidyr, readr, forcats
-library(readxl)
-library(janitor)
-library(here)
-library(cowplot)
-library(plotly)
-library(scales)
+# be sure to check for packages conflicts!
+# 01 import/export ----
+library(readxl) # read excel files
+library(janitor) # simple tools to clean dirty data
+library(here) # a simpler way to find your files
+library(SWMPr) # working with SWMP data from the NERRS
+library(xlsx) # to export df as .xlsx files
+
+# 02 tidy and wrangle ----
+library(tidyverse) # because...tidyverse (ggplot2, tidyr, dplyr)
+library(lubridate) # dates and times
+
+# 03 pulling information ----
+library(broom) # convert statistical analysis objects into tidy tibbles
+
+# 04 markdown ----
 library(knitr)
+library(rmarkdown)
 library(kableExtra) # https://cran.r-project.org/web/packages/kableExtra/vignettes/awesome_table_in_html.html 
-library(patchwork)
-library(broom)
-library(psych)
+
+# 05 graphics ----
+library(cowplot) # additional themes for ggplot2
+library(patchwork) # grid graphics
+library(scales) # scale functions for visualization
+library(plotly) # create interactive web graphics - use for html output files
+library(ggcorrplot) # visualization of correlation matrix using ggplot2
+# or install the latest ggcorrplot from GitHub
+# Install
+# if(!require(devtools)) install.packages("devtools")
+# devtools::install_github("kassambara/ggcorrplot")
