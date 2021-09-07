e1906d1e5ddd67a72bae96556473d7029e0db4e8
Dunnigan <Shannon.Dunnigan@dep.state.fl.us>
2021-09-07 15:08:30 -0400

    developing code to read in all historic and current year SWMP files for use in future code

--- R/01_load_wrangle.R
+++ R/01_load_wrangle.R
@@ -3,137 +3,158 @@
 # source('R/00_loadpackages.R')
 
 # 01 load data --------------------------------------------------
-## 01.1 load GTMNERR SWMP nutrient datasets
-## 01.2 load GTMNERR SWMP field data
+## 01.1 load 2002-2020 Nutrient Data
+## 01.2 load 2021 Nutrient Data
+## 01.3 load 2001-2020 WQ and MET files
+## 01.4 load 2021 WQ and MET files
 
 
-## 01.1 2021 nutrient data ------------------------------------------------------
+## 01.1 load 2002-2020 Nutrient Data ------------------------------------------------------
 
-nut_2021 <- readxl::read_xlsx(here::here('data', 
-                                         'gtmnut2021Q1-Q2.xlsx'), 
-                              sheet = "Data") %>% 
-            janitor::clean_names(case = "screaming_snake")
+nms <- names(read_excel(here::here('data',
+                                    '2001_2020_WQ_MET_NUT_FilesCDMO',
+                                    'All_inclusive_NUT',
+                                    'gtmnut2002-2020_QC.xlsx'), n_max = 0)) # pull out all the column names in this file
 
-# inspect the data
-glimpse(nut)
+class <- ifelse(grepl("^F_", nms), "text", "numeric") # read everything with F_ as a character
+class2 <- class[-(1:5)] # remove the first five elements of the vector because they are different
 
+nut_hist <- readxl::read_xlsx(here::here('data',
+                                         '2001_2020_WQ_MET_NUT_FilesCDMO',
+                                         'All_inclusive_NUT',
+                                         'gtmnut2002-2020_QC.xlsx'),
+                              col_types = c("text", "date", "numeric", "numeric", "text", class2))  # specify how to read in these columns
+            
+  # clean environment
+rm(nms, class, class2)
 
-## 01.2 2021 field data loop ----------------------------------------------------
+## 01.2 load 2021 Nutrient Data ------------------------------------------
+nms <- names(read_excel(here::here('data',
+                                   '2021',
+                                   'gtmnut2021.xlsx'),
+                        sheet = "Data", 
+                        n_max = 0)) # pull out all the column names in this file
 
-# initialize readin listing
-mysheets_fromexcel <- list()
-
-# get the list of all the sheet names
-mysheetlist <- readxl::excel_sheets(path = here::here('data', 
-                                                      '2021_FIELDDATA_v1.xlsx'))
-
-# create loop for the sheets
-i = 1
-
-for (i in 1:length(mysheetlist)){
-  
-  tempdf <- readxl::read_excel(path = here::here('data', 
-                                                 '2021_FIELDDATA_v1.xlsx'), 
-                               sheet = mysheetlist[i])
-  
-  tempdf$sheetname <- mysheetlist[i]
-  
-  mysheets_fromexcel[[i]] <- tempdf 
-}
-
-mysheets_fromexcel
-
-
-# merge all the lists into one tibble using dplyr::bind_rows()
-env <- purrr::reduce(mysheets_fromexcel, dplyr::bind_rows) %>% 
-  janitor::clean_names()
-
-# inspect the data
-glimpse(env)
-
-# clear environment
-
-rm(mysheets_fromexcel, 
-   tempdf, i, mysheetlist)
-
-
-## 01.3 2020 nut data ------------------------------------------------------
-nut_2020 <- readxl::read_xlsx(here::here('data', 
-                                         '2002-2020', 
-                                         'gtmnut2020.xlsx'), 
-                              sheet = "Data") %>% 
-            janitor::clean_names(case = "screaming_snake")
-
-
-## 01.4 2002-2019 nut data -------------------------------------------------
-nut_2002_2019 <- readxl::read_xlsx(here::here('data', 
-                                              '2002-2020', 
-                                              'gtmnut2002-2019_QC.xlsx'), 
-                                  ) %>% 
-                 janitor::clean_names(case = "screaming_snake")
+class <- ifelse(grepl("^F_", nms), "text", "numeric") # read everything with F_ as a character
+class2 <- class[-(1:5)] # remove the first five elements of the vector because they are different
 
 
 
-# 02 wrangle-tidy data ------------------------------------------------------
+nut_2021 <- readxl::read_xlsx(here::here('data',
+                                         '2021',
+                                         'gtmnut2021.xlsx'),
+                              sheet = 'Data',
+                              col_types = c("text", "text", "date", "numeric", "numeric", class2)) # specify how to read in these columns)
+# clean environment 
+rm(nms, class, class2)
+
+## 01.3 load 2001-2020 WQ and MET files ------------------------------------------
+## import data with `SWMPr::import_local()` and then clean it with `SWMPr::qaqc()` to screen observations
+## check what the flags mean used in the `SWMPr::qaqc()` fxn here:  https://cdmo.baruch.sc.edu/data/qaqc.cfm.
+## add in station name (for combining)
+pi_hist <- SWMPr::import_local(path = here::here('data',
+                                          '2001_2020_WQ_MET_NUT_FilesCDMO'), 
+                               station_code = 'gtmpiwq') %>% 
+           SWMPr::qaqc(qaqc_keep = c('0', '1', '2', '3', '4', '5')) %>% 
+           dplyr::mutate(station = 'gtmpiwq')
+ss_hist <- SWMPr::import_local(path = here::here('data',
+                                                 '2001_2020_WQ_MET_NUT_FilesCDMO'), 
+                               station_code = 'gtmsswq') %>% 
+           SWMPr::qaqc(qaqc_keep = c('0', '1', '2', '3', '4', '5')) %>% 
+           dplyr::mutate(station = 'gtmsswq')
+fm_hist <- SWMPr::import_local(path = here::here('data',
+                                                 '2001_2020_WQ_MET_NUT_FilesCDMO'), 
+                               station_code = 'gtmfmwq') %>% 
+           SWMPr::qaqc(qaqc_keep = c('0', '1', '2', '3', '4', '5')) %>% 
+           dplyr::mutate(station = 'gtmfmwq')
+pc_hist <- SWMPr::import_local(path = here::here('data',
+                                                 '2001_2020_WQ_MET_NUT_FilesCDMO'), 
+                               station_code = 'gtmpcwq') %>% 
+           SWMPr::qaqc(qaqc_keep = c('0', '1', '2', '3', '4', '5')) %>% 
+           dplyr::mutate(station = 'gtmpcwq')
+
+WQ_hist <- dplyr::bind_rows(pi_hist, ss_hist, fm_hist, pc_hist)
+
+MET_hist <- SWMPr::import_local(path = here::here('data',
+                                                  '2001_2020_WQ_MET_NUT_FilesCDMO'), 
+                                station_code = 'gtmpcmet') %>% 
+            SWMPr::qaqc(qaqc_keep = c('0', '1', '2', '3', '4', '5')) %>% 
+            dplyr::mutate(station = 'gtmpcmet')
+
+# choose to keep or remove individual stations
+rm(pi_hist, ss_hist, fm_hist, pc_hist)
+
+## 01.4 load 2021 WQ and MET files ------------------------------------------
+pi_2021 <- SWMPr::import_local(path = here::here('data',
+                                                 '2021',
+                                                 'WQ-MET'), 
+                               station_code = 'gtmpiwq') %>% 
+           SWMPr::qaqc(qaqc_keep = c('0', '1', '2', '3', '4', '5')) %>% 
+           dplyr::mutate(station = 'gtmpiwq')
+ss_2021 <- SWMPr::import_local(path = here::here('data',
+                                                 '2021',
+                                                 'WQ-MET'), 
+                               station_code = 'gtmsswq') %>% 
+           SWMPr::qaqc(qaqc_keep = c('0', '1', '2', '3', '4', '5')) %>% 
+           dplyr::mutate(station = 'gtmsswq')
+fm_2021 <- SWMPr::import_local(path = here::here('data',
+                                                 '2021',
+                                                 'WQ-MET'), 
+                               station_code = 'gtmfmwq') %>% 
+           SWMPr::qaqc(qaqc_keep = c('0', '1', '2', '3', '4', '5')) %>% 
+           dplyr::mutate(station = 'gtmfmwq')
+pc_2021 <- SWMPr::import_local(path = here::here('data',
+                                                 '2021',
+                                                 'WQ-MET'), 
+                               station_code = 'gtmpcwq') %>% 
+           SWMPr::qaqc(qaqc_keep = c('0', '1', '2', '3', '4', '5')) %>% 
+           dplyr::mutate(station = 'gtmpcwq')
+
+WQ_2021 <- dplyr::bind_rows(pi_2021, ss_2021, fm_2021, pc_2021)
+
+MET_2021 <- SWMPr::import_local(path = here::here('data',
+                                                  '2021',
+                                                  'WQ-MET'), 
+                                station_code = 'gtmpcmet') %>% 
+            SWMPr::qaqc(qaqc_keep = c('0', '1', '2', '3', '4', '5')) %>% 
+            dplyr::mutate(station = 'gtmpcmet')
+
+# choose to keep or remove individual stations
+rm(pi_2021, ss_2021, fm_2021, pc_2021)
 
 
-## 02.1 combine 2020-2021 files --------------------------------------------
-
-nut_2020_2021 <- bind_rows(nut_2020, nut_2021)
-
-glimpse(nut_2020_2021)
+# 02 wrangle data for merging ------------------------------------------------
+## 02.1 Nutrients
+## 02.2 WQ and MET
 
 
+## 02.1 Nutrient file wrangling --------------------------------------------
 
-## 02.2 combine 2002-2021 --------------------------------------------------
+# # check the two data frames using `janitor::compare_df_cols()`
+# janitor::compare_df_cols(nut_hist, nut_2021,
+#                          bind_method = "bind_rows",
+#                          return = "mismatch")
 
-janitor::compare_df_cols_same(nut_2002_2019, nut_2020_2021,
-                              bind_method = "bind_rows")
-
-nut_2002_2019 <- nut_2002_2019 %>% 
-                  dplyr::mutate(DOC = as.numeric(DOC),
-                                DON = as.numeric(DON),
-                                ENTERO_MPN = as.numeric(ENTERO_MPN),
-                                F_DOC = as.character(F_DOC),
-                                F_DON = as.character(F_DON),
-                                F_ENTERO_MPN = as.character(F_ENTERO_MPN),
-                                F_FECCOL_CFU = as.character(F_FECCOL_CFU),
-                                F_PON = as.character(F_PON),
-                                F_TKN = as.character(F_TKN),
-                                F_TKNF = as.character(F_TKNF),
-                                F_TON = as.character(F_TON),
-                                FECCOL_CFU = as.numeric(FECCOL_CFU),
-                                PON = as.numeric(PON),
-                                REP = as.numeric(REP),
-                                TKN = as.numeric(TKN),
-                                TKNF = as.numeric(TKNF),
-                                TON = as.numeric(TON)
-                  )
-
-nut_2020_2021 <- nut_2020_2021 %>% 
-                  dplyr::mutate(F_PH_N = as.character(F_PH_N),
-                                F_SALT_N = as.character(F_SALT_N),
-                                F_WTEM_N = as.character(F_WTEM_N))
-
-nut <- bind_rows(nut_2002_2019, nut_2020_2021)
-
-## 02.1 wrangle-tidy field data ---------------------------------------------
-
-env2 <- env %>%
-  dplyr::mutate(date_sampled = lubridate::ymd_hm(paste(date, time_24_hr)),
-                site = tolower(site),
-                component_long = tolower(component_long),
-                cdmo_name = forcats::as_factor(component_short)
-                ) %>% 
-  dplyr::select(station_code, date_sampled, cdmo_name, component_long, result, remark) %>% 
-  tidyr::separate(station_code, 
-                  into = c("station_code", "num"), 
-                  sep = "(?<=[A-Za-z])(?=[0-9])") %>%
-  tidyr::separate(num,
-                  into = c("monitoringprogram", "replicate"),
-                  sep = "[.]")
+# make some changes to the 2021 data to match the hist data:
+nut_2021 <- nut_2021 %>% 
+              dplyr::rename(COLOR = Color, 
+                            F_COLOR = F_Color,
+                            REP = Rep) %>% 
+              dplyr::select(-`Station Code_GTM`)
 
 
-# merge env and nut -------------------------------------------------------
+# 03 combine historic and 2021 files --------------------------------------
+## 03.1 Nutrient
+## 03.2 WQ and MET
 
-dat <- dplyr::bind_rows(nut2, env2)
+## 03.1 Nutrient files merge -----------------------------------------------
+# `bind_rows()` the hist and 2021 data and also clean up the column names using `clean_names()`
+NUT <- dplyr::bind_rows(nut_hist, nut_2021) %>% 
+  janitor::clean_names(case = "screaming_snake")
+
+# view the datafile
+dplyr::glimpse(NUT)
+
+# clean up environment
+rm(nut_hist, nut_2021)
+
